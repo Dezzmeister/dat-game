32d432517894bfb834c0c24f6bc032629939cdcc
Dezzmeister <dezzy3359@gmail.com>
2018-01-15 19:16:50 -0500

    Update prebuild.bat

--- prebuild.bat
+++ prebuild.bat
@@ -1,4 +1,6 @@
+
 REM To use this file correctly, replace the path below "C:\java\eclipse-workspace\LinegameEditor2\src" with YOUR OWN path
 REM to LinegameEditor2\src. Do not expect it to work if you do not change this.
 java -jar prebuild.jar C:\java\eclipse-workspace\LinegameEditor2\src
+javac -source 10 -d bin @args.txt
 exit
