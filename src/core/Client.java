678eec373260c3afe35e6dbc4ff87274c388f27a
Dezzmeister <dezzy3359@gmail.com>
2018-01-15 20:45:45 -0500

    Update Client.java

--- src/core/Client.java
+++ src/core/Client.java
@@ -1,11 +1,13 @@
 package core;
 
+import java.util.List;
+
 public class Client {
 
 	public static void main(String[] args) {
 		var a = 5;
 		System.out.println(a);
+		var immutable = List.of("a", "b", "c");
+		immutable.forEach(s -> System.out.println(s));
 	}
-	
-	
 }
